30775d85cc4172cea6a11f10a41e0cb91c4cc026
jh-chch <jhchoig9@gmail.com>
2025-07-05 20:43:14 +0900

    init

---
+++ src/main/java/com/example/matchingplatform/MatchingplatformApplication.java
@@ -0,0 +1,13 @@
+package com.example.matchingplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatchingplatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatchingplatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/matchingplatform/MatchingplatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.matchingplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatchingplatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
